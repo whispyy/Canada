2c8004bb79b5a5b06b9662c9d3efeecdda24dbd2
JF <jf@localhost.localdomain>
2017-06-06 03:51:06 +0200

    init the project

---
+++ app/ts/app.component.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/core';
+
+import {Home} from './components/home/home';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>Angular 2 Template</h1>'
+})
+
+export class AppComponent {}
