1cbf7b71f14aa17e9a49bba3a61a83fee1f88f7d
JF <jfdurk@gmx.fr>
2017-06-29 17:15:10 -0400

    init list component

---
+++ app/components/list/list.component.ts
@@ -0,0 +1,12 @@
+import {Component} from 'angular2/core';
+
+@Component({
+	selector: 'list',
+	templateUrl: 'app/components/list/list.component.html',
+	providers: []
+
+})
+
+export class ListComponent {
+
+}
--- app/home/home.ts
+++ app/home/home.ts
@@ -3,12 +3,13 @@ import {Component, Output} from 'angular2/core';
 // components
 import {CardComponent} from '../components/card/card.component';
 import {NavBarComponent} from '../components/nav-bar/nav-bar.component';
+import {ListComponent} from '../components/list/list.component';
 
 @Component({
 	selector: 'home',
 	templateUrl: 'app/home/home.html',
 	styleUrls: ['app/home/home.css'],
-	directives: [CardComponent, NavBarComponent],
+	directives: [CardComponent, NavBarComponent, ListComponent],
 	providers: []
 
 })
